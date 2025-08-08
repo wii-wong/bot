a45aa4818d147410f2ec124760e666664f5cae09
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2025-08-07 18:49:46 -0700

    plant seeds

--- src/plantSeeds.ts
+++ src/plantSeeds.ts
@@ -1,5 +1,9 @@
-import { objectsByName } from "@dust/world/internal";
+import { objectsByName, packVec3, Vec3 } from "@dust/world/internal";
 import { SyncToStashResult } from "@latticexyz/store-sync/internal";
+import { worldContract } from "./chain";
+import { lowerFarmCoord, upperFarmCoord } from "./constants";
+import { getObjectsInArea } from "./getObjectsInArea";
+import { getObjectTypeAt } from "./getObjectTypeAt";
 import { PlayerInfo } from "./getPlayerInfo";
 import { getSlotsWithObject } from "./getSlotsWithObject";
 
@@ -15,7 +19,46 @@ export async function plantSeeds({
     console.warn("No seeds found in inventory.");
     return;
   }
-  console.log(`Planting ${seeds.length} seeds...`);
+  console.log(
+    `Planting ${seeds.reduce((total, seed) => total + seed.amount, 0)} seeds...`
+  );
+  const wetFarmlands = await getObjectsInArea(
+    lowerFarmCoord,
+    upperFarmCoord,
+    objectsByName.WetFarmland.id
+  );
+  if (wetFarmlands.length === 0) {
+    console.warn("No wet farmlands found to plant seeds.");
+    return;
+  }
+  for (const wetFarmland of wetFarmlands) {
+    const seed = seeds.at(0);
+    if (!seed) {
+      console.warn("No seeds left to plant.");
+      break;
+    }
+
+    const plantPos: Vec3 = [wetFarmland[0], wetFarmland[1] + 1, wetFarmland[2]];
+    const objectType = await getObjectTypeAt(plantPos);
+    if (objectType !== objectsByName.Air.id) {
+      continue;
+    }
+    console.log(`Planting seed at ${plantPos} with slot ${seed.slot}`);
+    const txHash = await worldContract.write.build([
+      player.entityId,
+      packVec3(plantPos),
+      seed.slot,
+      "0x",
+    ]);
+    await stashResult.waitForTransaction(txHash);
+    console.log(
+      `Planted seed at ${plantPos} with slot ${seed.slot}, txHash: ${txHash}`
+    );
+    seed.amount -= 1;
+    if (seed.amount === 0) {
+      seeds.shift(); // Remove the seed slot if no more seeds left
+    }
+  }
 
   console.log("Seeds planted!");
 }
