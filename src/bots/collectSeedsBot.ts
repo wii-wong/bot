72bc82afeb10fa7649a5bd8267187aacaf879c83
wii.wong <wii.wong.1995@gmail.com>
2025-10-14 17:29:32 +0800

    feat: implement collectSeedsBot to automate seed collection and tool management

---
+++ src/bots/collectSeedsBot.ts
@@ -0,0 +1,42 @@
+import { ObjectName } from "@dust/world/internal";
+import { InteractWithChest } from "../tasks/InteractWithChest";
+import { mineResources } from "../tasks/mineResources";
+import { movePlayer } from "../tasks/movePlayer";
+import { returnToolsToChest } from "../tasks/returnToolsToChest";
+import { takeTools } from "../tasks/takeTools";
+import { BotContext, ToleranceType, WorldRegion } from "../types";
+import { RESOURCE_CHEST_POSITION, SEEDS_CHEST_POSITION } from "../utils/constants";
+
+/// SEEDS
+const searchRadius = 10;
+const searchRegion: WorldRegion = {
+  topLeftCoord: [250, 80, -3000],
+  bottomRightCoord: [800, 28, -2500],
+}
+const searchItem: ObjectName = "SwitchGrass";
+const toolsAvailble = ["DiamondPick"] as ObjectName[];
+const maxTotalTools = 10;
+const waitForTxn = true;
+
+export async function collectSeedsBot(context: BotContext) {
+  // Step 1: Get tools from chest
+  await takeTools({ toolsAvailble, maxTotalTools }, context);
+
+  // Step 2: Main mining loop
+  await mineResources({ searchRegion, searchRadius, searchItem, toolsAvailble, ignoreTimeLimit: true, waitingForTxn: waitForTxn }, context);
+
+  // Step 3: place resources into chest
+  await movePlayer(RESOURCE_CHEST_POSITION, context, {
+    toleranceType: ToleranceType.Cube,
+    tolerance: 5,
+    avoidBlocks: ["Lava", "Water"],
+  });
+  await InteractWithChest({ objectName: searchItem, chestCoord: RESOURCE_CHEST_POSITION, amount: 10e8, action: "deposit" }, context);
+  await InteractWithChest({ objectName: "WheatSeed", chestCoord: SEEDS_CHEST_POSITION, amount: 10e8, action: "deposit" }, context);
+  await InteractWithChest({ objectName: "MelonSeed", chestCoord: SEEDS_CHEST_POSITION, amount: 10e8, action: "deposit" }, context);
+  await InteractWithChest({ objectName: "PumpkinSeed", chestCoord: SEEDS_CHEST_POSITION, amount: 10e8, action: "deposit" }, context);
+
+  // Step 4: Return all tools to the chest
+  await returnToolsToChest(context, toolsAvailble);
+}
+
